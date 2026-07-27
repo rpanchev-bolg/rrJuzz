50cb7a88defdaf8222dd3fe2e21efb55d83474d3
Panchev Roman Maksimovich <r.panchev@gmail.com>
2026-07-28 01:29:59 +0300

    Получение курсов валют из json в консоли с PrettyPrinter.

--- Задание_5.py
+++ Задание_5.py
@@ -5,8 +5,7 @@ import pprint
 # настраиваем PrettyPrinter
 # Очень полезная штука, разбивает вложения json на форматированные блоки
 p = pprint.PrettyPrinter(indent=4)  # indent=4 - значит отступ в 4 пробела
-
 result = requests.get('https://open.er-api.com/v6/latest/USD')
-
 data = json.loads(result.text)
+
 p.pprint(data)  # "красиво" выводим на экран данные
